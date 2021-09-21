8a1e4c69fc1330f55a45285c2c71c52dd583e130
Matheus Alexandre <alexandremhm@gmail.com>
2021-09-21 14:56:16 -0300

    Retira gabarito

--- tests/magic.test.js
+++ tests/magic.test.js
@@ -3,36 +3,21 @@ const favoriteCards = require('../data/favoriteCards.js');
 
 const retrievesFavoriteCards = () => {
   // implemente sua função aqui
-  // favoriteCards.splice(4, favoriteCards.length - 4);
 }
 
-describe(' Testa a função getMagicCard', () => {
-  // afterEach(() => {
-  //   retrievesFavoriteCards();
-  // });
+describe(' Testa a função getMagicCard', () => { 
 
   it('Testa se um novo card é adicionado a cada execução', async () => {
 
     // expect.assertions(4);
-    // const response = await getMagicCard('130553');
-
-    expect(favoriteCards).toHaveLength(5);
-    expect(favoriteCards[4].name).toEqual('Beacon of Immortality');
-
-    const responseTwo = await getMagicCard('130554');
-
-    expect(favoriteCards).toHaveLength(6);
-    expect(favoriteCards[5].name).toEqual('Cho-Manno, Revolutionary');   
+    // const response = await getMagicCard('130553');    
 
   });
 
   it('Deve retornar favoriteCards contendo apenas os cards favoritos iniciais', () => {
 
-    expect.assertions(2);
-    expect(favoriteCards).toHaveLength(4);
-
-    const namesOfFavoriteCards = favoriteCards.map(card => card.name);
-    expect(namesOfFavoriteCards).toEqual(['Ancestor\'s Chosen', 'Angel of Mercy', 'Aven Cloudchaser', 'Ballista Squad']);
+    // expect.assertions(2);
+   
 
   })
 });
